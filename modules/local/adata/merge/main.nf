29946093fbca894051c45e8f34bcb2ef4a956428
Nico Trummer <nictru32@gmail.com>
2024-06-09 06:22:01 +0000

    Implement adata merge process

---
+++ modules/local/adata/merge/main.nf
@@ -0,0 +1,24 @@
+process ADATA_MERGE {
+    tag "$meta.id"
+    label 'process_single'
+
+    conda "${moduleDir}/environment.yml"
+    container "${ workflow.containerEngine == 'singularity' && !task.ext.singularity_pull_docker_container ?
+        'oras://community.wave.seqera.io/library/scanpy:1.10.1--ea08051addf267ac':
+        'community.wave.seqera.io/library/scanpy:1.10.1--0c8c97148fc05558' }"
+
+    input:
+    tuple val(meta), path(h5ads)
+
+    output:
+    tuple val(meta), path("*_inner.h5ad"), emit: inner
+    tuple val(meta), path("*_outer.h5ad"), emit: outer
+    path "versions.yml"                  , emit: versions
+
+    when:
+    task.ext.when == null || task.ext.when
+
+    script:
+    prefix = task.ext.prefix ?: "${meta.id}"
+    template 'merge.py'
+}
---
+++ modules/local/adata/merge/templates/merge.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import scanpy as sc
+import anndata as ad
+from scipy.sparse import csr_matrix
+import platform
+import scipy
+
+def format_yaml_like(data: dict, indent: int = 0) -> str:
+    """Formats a dictionary to a YAML-like string.
+
+    Args:
+        data (dict): The dictionary to format.
+        indent (int): The current indentation level.
+
+    Returns:
+        str: A string formatted as YAML.
+    """
+    yaml_str = ""
+    for key, value in data.items():
+        spaces = "  " * indent
+        if isinstance(value, dict):
+            yaml_str += f"{spaces}{key}:\\n{format_yaml_like(value, indent + 1)}"
+        else:
+            yaml_str += f"{spaces}{key}: {value}\\n"
+    return yaml_str
+
+adatas = [sc.read_h5ad(f) for f in "${h5ads}".split()]
+genes = [adata.var_names for adata in adatas]
+
+adata_outer = ad.concat(adatas, join="outer")
+adata_outer.X = csr_matrix(adata_outer.X)
+adata_outer.layers["counts"] = adata_outer.X
+
+# Make sure there are no cells and genes without any counts
+sc.pp.filter_cells(adata_outer, min_counts=1)
+sc.pp.filter_genes(adata_outer, min_cells=1)
+
+genes_intersection = set(adata_outer.var_names).intersection(*genes)
+adata_inner = adata_outer[:, genes_intersection]
+
+adata_inner.write("${prefix}_inner.h5ad")
+adata_outer.write("${prefix}_outer.h5ad")
+
+# Versions
+
+versions = {
+    "${task.process}": {
+        "python": platform.python_version(),
+        "scanpy": sc.__version__,
+        "anndata": ad.__version__,
+        "scipy": scipy.__version__,
+    }
+}
+
+with open("versions.yml", "w") as f:
+    f.write(format_yaml_like(versions))
