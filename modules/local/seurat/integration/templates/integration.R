410420d50bb360925c2eada9b97928287dfd1133
Nico Trummer <nictru32@gmail.com>
2024-06-15 12:00:22 +0200

    Improve seurat script

--- modules/local/seurat/integration/templates/integration.R
+++ modules/local/seurat/integration/templates/integration.R
@@ -4,37 +4,25 @@ library(Seurat)
 
 sce <- readRDS("${rds}")
 
-seurat_obj <- as.Seurat(sce)
+seurat_obj <- as.Seurat(sce, data = NULL)
+seurat_obj <- seurat_obj[, unname(which(colSums(GetAssayData(seurat_obj)) != 0))]
 
-batch_list <- SplitObject(seurat_obj, split.by = "batch")
+batches <- SplitObject(seurat_obj, split.by = "batch")
+batches <- lapply(X = batches, FUN = SCTransform, assay = "originalexp")
+
+features <- SelectIntegrationFeatures(object.list = batches, nfeatures = 2000)
+batches <- PrepSCTIntegration(object.list = batches, anchor.features = features)
+batches <- lapply(X = batches, FUN = RunPCA, features = features, verbose=F)
 
 anchors <- FindIntegrationAnchors(
-  object.list = batch_list,
-  anchor.features = 2000,
-  scale = T,
-  l2.norm = T,
-  dims = 1:30,
-  k.anchor = 5,
-  k.filter = 200,
-  k.score = 30,
-  max.features = 200,
-  eps = 0
+    object.list = batches,
+    normalization.method = "SCT",
+    anchor.features = features
 )
 
 integrated <- IntegrateData(
-  anchorset = anchors,
-  new.assay.name = "X_emb",
-  features = NULL,
-  features.to.integrate = NULL,
-  dims = 1:30,
-  k.weight = 100,
-  weight.reduction = NULL,
-  sd.weight = 1,
-  sample.tree = NULL,
-  preserve.order = F,
-  do.cpp = T,
-  eps = 0,
-  verbose = T
+    anchorset = anchors,
+    normalization.method = "SCT"
 )
 
 saveRDS(integrated, "${prefix}.rds")
