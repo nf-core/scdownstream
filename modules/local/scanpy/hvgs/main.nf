fe1476f5ad8c35162af9062c614d9670538b971a
Nico Trummer <nictru32@gmail.com>
2024-08-01 14:23:54 +0200

    Implement integration HVG selection

---
+++ modules/local/scanpy/hvgs/main.nf
@@ -0,0 +1,24 @@
+process SCANPY_HVGS {
+    tag "$meta.id"
+    label 'process_single'
+
+    conda "${moduleDir}/environment.yml"
+    container "${ workflow.containerEngine == 'singularity' && !task.ext.singularity_pull_docker_container ?
+        'oras://community.wave.seqera.io/library/python-igraph_scanpy:a7114d55b0af3893':
+        'community.wave.seqera.io/library/python-igraph_scanpy:5f677450e42211ef' }"
+
+    input:
+    tuple val(meta), path(h5ad)
+    val(n_hvgs)
+
+    output:
+    tuple val(meta), path("*.h5ad"), emit: h5ad
+    path "versions.yml"            , emit: versions
+
+    when:
+    task.ext.when == null || task.ext.when
+
+    script:
+    prefix = task.ext.prefix ?: "${meta.id}"
+    template 'hvgs.py'
+}
---
+++ modules/local/scanpy/hvgs/templates/hvgs.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import scanpy as sc
+import platform
+
+from threadpoolctl import threadpool_limits
+threadpool_limits(int("${task.cpus}"))
+sc.settings.n_jobs = int("${task.cpus}")
+
+def format_yaml_like(data: dict, indent: int = 0) -> str:
+    """Formats a dictionary to a YAML-like string.
+
+    Args:
+        data (dict): The dictionary to format.
+        indent (int): The current indentation level.
+
+    Returns:
+        str: A string formatted as YAML.
+    """
+    yaml_str = ""
+    for key, value in data.items():
+        spaces = "  " * indent
+        if isinstance(value, dict):
+            yaml_str += f"{spaces}{key}:\\n{format_yaml_like(value, indent + 1)}"
+        else:
+            yaml_str += f"{spaces}{key}: {value}\\n"
+    return yaml_str
+
+
+adata = sc.read_h5ad("${h5ad}")
+prefix = "${prefix}"
+n_hvgs = int("${n_hvgs}")
+
+if adata.n_vars > n_hvgs:
+    sc.pp.highly_variable_genes(adata,
+                                n_top_genes=int("${n_hvgs}"),
+                                batch_key="batch",
+                                layer="log1p")
+    adata = adata[:, adata.var["highly_variable"]]
+
+adata.write_h5ad(f"{prefix}.h5ad")
+
+# Versions
+
+versions = {
+    "${task.process}": {
+        "python": platform.python_version(),
+        "scanpy": sc.__version__
+    }
+}
+
+with open("versions.yml", "w") as f:
+    f.write(format_yaml_like(versions))
