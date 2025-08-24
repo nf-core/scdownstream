678438d5718fd1279670c0001a5a1380a8a6ff66
ECM893 <eli.mcpherson1@gmail.com>
2025-08-24 16:25:44 -0400

    Hack to fix celltypist to scanvi cell label column issue.

--- modules/local/scvitools/scanvi/templates/scanvi.py
+++ modules/local/scvitools/scanvi/templates/scanvi.py
@@ -25,6 +25,40 @@ adata = ad.read_h5ad("${h5ad}")
 reference_model_path = "reference_model"
 reference_model_type = "${meta2.id}"
 
+# FIXME: This is a hack, the columns from celltypist are:
+# f"celltypist:{model_name}", f"celltypist:{model_name}:conf" (from line 69)
+# But here it expects the labels under "label" currently it seems that no
+# script triggered is handling confidence cutoff and label assignment to
+# "${label_col}" ("label").
+# Here I hack a solution in the event celltypist is used:
+# Overwrite labels with CellTypist predictions (with confidence filter)
+# Find any CellTypist prediction/confidence columns
+pred_cols = [
+    c
+    for c in adata.obs.columns
+    if c.startswith("celltypist:") and not c.endswith(":conf")
+]
+
+# If multiple models are present, I will pick the first one
+pred_col = pred_cols[0]
+conf_col = pred_col + ":conf"
+
+print(f"Using CellTypist column: {pred_col}")
+
+if conf_col in adata.obs:
+    CUTOFF = 0.7
+    adata.obs["${label_col}"] = adata.obs.apply(
+        lambda r: r[pred_col] if r[conf_col] >= CUTOFF else "unknown", axis=1
+    )
+else:
+    adata.obs["${label_col}"] = adata.obs[pred_col]
+
+# Make sure labels are categorical and contain "unknown"
+adata.obs["${label_col}"] = adata.obs["${label_col}"].astype("category")
+if "unknown" not in adata.obs["${label_col}"].cat.categories:
+    adata.obs["${label_col}"] = adata.obs["${label_col}"].cat.add_categories(["unknown"])
+# ---- End Hack ----
+
 if reference_model_type == "scanvi":
     SCANVI.prepare_query_anndata(adata, reference_model_path)
     model = SCANVI.load_query_data(adata, reference_model_path)
