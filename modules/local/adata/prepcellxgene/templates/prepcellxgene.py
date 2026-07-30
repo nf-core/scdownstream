2b45368ba267dee96e74131ff810bc868462a72f
Nico Trummer <nictru32@gmail.com>
2026-07-30 18:30:29 +0200

    Improve cellxgene integration ordering

--- modules/local/adata/prepcellxgene/templates/prepcellxgene.py
+++ modules/local/adata/prepcellxgene/templates/prepcellxgene.py
@@ -20,12 +20,20 @@ from scipy.sparse import csc_matrix
 adata = ad.read_h5ad("${h5ad}")
 
 integration_methods = ["scanvi", "scvi", "symphony", "scimilarity", "seurat", "bbknn", "combat", "pca", "expimap"]
-
-dim_reds = ["umap", "tsne"]
+dimred_methods = ["umap", "tsne"]
 
 for key in list(adata.obsm.keys()):
-    if not any(key.endswith(dim_red) for dim_red in dim_reds):
+    if not any(key.endswith(dimred_method) for dimred_method in dimred_methods):
         del adata.obsm[key]
+
+found_index = 1
+for integration_method in integration_methods:
+    for dimred_method in dimred_methods:
+        key = f"X_{integration_method}-global_{dimred_method}"
+        if key in adata.obsm.keys():
+            new_key = f"X_{found_index}_{integration_method}-global_{dimred_method}"
+            adata.obsm[new_key] = adata.obsm.pop(key)
+            found_index += 1
 
 # Delete everything in uns and layers (CELLxGENE can't display these anyway)
 adata.uns = {}
